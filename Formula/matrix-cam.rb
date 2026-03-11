dcc16fcc64ef954a92ee1302fabf92e16164484e
Mico Papp <papp.mico@gmail.com>
2026-03-11 15:33:08 +0100

    matrix-cam dependency update

--- Formula/matrix-cam.rb
+++ Formula/matrix-cam.rb
@@ -14,6 +14,20 @@ class MatrixCam < Formula
 
   depends_on "python@3.11"
 
+  resource "numpy" do
+    on_macos do
+      on_arm do
+        url "https://files.pythonhosted.org/packages/b5/7c/c061f3de0630941073d2598dc271ac2f6cbcf5c83c74a5870fea07488333/numpy-2.4.3-cp311-cp311-macosx_11_0_arm64.whl"
+        sha256 "8ba7b51e71c05aa1f9bc3641463cd82308eab40ce0d5c7e1fd4038cbf9938147"
+      end
+
+      on_intel do
+        url "https://files.pythonhosted.org/packages/f9/51/5093a2df15c4dc19da3f79d1021e891f5dcf1d9d1db6ba38891d5590f3fe/numpy-2.4.3-cp311-cp311-macosx_10_9_x86_64.whl"
+        sha256 "33b3bf58ee84b172c067f56aeadc7ee9ab6de69c5e800ab5b10295d54c581adb"
+      end
+    end
+  end
+
   resource "opencv-python" do
     on_macos do
       on_arm do
@@ -34,13 +48,19 @@ class MatrixCam < Formula
 
     ENV["PIP_ONLY_BINARY"] = "numpy,opencv-python"
 
-    opencv = resource("opencv-python")
-    opencv.fetch
-    wheel_path = Pathname.new(opencv.cached_download)
-    wheel_copy = buildpath/File.basename(opencv.url)
-    cp wheel_path, wheel_copy
-    venv.pip_install wheel_copy
+    install_wheel_resource "numpy", venv
+    install_wheel_resource "opencv-python", venv
     venv.pip_install_and_link buildpath
+  end
+
+  private
+
+  def install_wheel_resource(name, venv)
+    resource = self.resource(name)
+    resource.fetch
+    wheel_copy = buildpath/File.basename(resource.url)
+    cp Pathname.new(resource.cached_download), wheel_copy
+    venv.pip_install wheel_copy
   end
 
   test do
